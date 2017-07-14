efdb35211f9bb1073ee2e9d677059d682cb5f8ee
Kip Warner <kip@thevertigo.com>
2017-07-13 18:34:28 -0700

    More work on the Narayan Logic parser...

---
+++ Source/NarayanLogicParserPreinclude.h
@@ -0,0 +1,23 @@
+/*
+    Narayan Designer, a modelling tool for the Narayan simulation engine.
+    Copyright (C) 2017 Cartesian Theatre. All rights reserved.
+*/
+
+// Multiple include protection...
+#ifndef _NARAYAN_LOGIC_PARSER_PREINCLUDE_H_
+#define _NARAYAN_LOGIC_PARSER_PREINCLUDE_H_
+
+// Includes...
+
+    // Provided by Autoconf...
+    #include <config.h>
+
+    // Standard C++ / POSIX system headers...
+    #include <cmath>
+    #include <string>
+    #include <utility>
+    #include <type_traits>
+
+// Multiple include protection...
+#endif
+
